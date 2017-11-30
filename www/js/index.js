529db5bfdb8c3a2f95cf8c33942abb3527b9c16e
marlon ruiz <marlon_ru@hotmail.com>
2017-11-30 14:30:39 -0600

    chat

--- www/js/index.js
+++ www/js/index.js
@@ -35,6 +35,7 @@ var app = {
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
 		app.initApplozicChat();
+		StatusBar.hide()
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
